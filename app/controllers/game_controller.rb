70b6102d9761749c205cbe4d2d088a159366c195
kv-2 <str_token@mail.ru>
2019-06-12 21:00:37 +0300

    update game controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,13 @@
 class GameController < ApplicationController
   def ping
-    logger.info ' +++ I go to ping +++ '
+    logger.info ' +++ I go to pong +++ '
     #redirect_to game_pong_path
-    redirect_to :action => 'pong'
+    redirect_to :action => 'pong'  
+    
   end
 
   def pong
+    logger.info ' +++ I go to back ping +++ '
+    #redirect_to :back
   end
 end
