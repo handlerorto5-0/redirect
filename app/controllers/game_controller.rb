1754cbb15826dc08a0009fd2afdeadecfd1282de
kv-2 <str_token@mail.ru>
2019-06-12 20:41:30 +0300

    add pmg redirect_to pong

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,8 @@
 class GameController < ApplicationController
   def ping
+    logger.info ' +++ I go to ping +++ '
+    #redirect_to game_pong_path
+    redirect_to :action => 'pong'
   end
 
   def pong
