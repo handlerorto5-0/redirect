3f8b8a2727920926450cf0cb48fbec8927ddc023
kv-2 <str_token@mail.ru>
2019-06-20 13:41:18 +0300

    add fladh

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,13 +1,10 @@
 class GameController < ApplicationController
   def ping
-    logger.info ' +++ I go to pong +++ '
-    #redirect_to game_pong_path
-    redirect_to :action => 'pong'  
+    redirect_to game_pong_path , notice: 'Ping-Pong'
     
   end
 
   def pong
-    logger.info ' +++ I go to back ping +++ '
-    #redirect_to :back
+
   end
 end
